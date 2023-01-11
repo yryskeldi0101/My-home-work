929e601c794e532e2ba43d8c4ac3561fe7aa5f83
yryskeldi0101 <yryskeldi.9121@gmail.com>
2023-01-11 18:34:30 +0600

    Home-work-5

--- src/App.js
+++ src/App.js
@@ -3,22 +3,76 @@ import "./App.css";
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/new-expenses/NewExpense";
 
-
+const items = [
+  {
+    id: 1,
+    title: 'Fruits',
+    price: 300,
+    date: new Date(2021, 2, 28)
+  },
+  {
+    id: 2,
+    title: 'Food',
+    price: 400,
+    date: new Date(2021, 5, 12)
+  },
+  {
+    id: 3,
+    title: 'Tea',
+    price: 300,
+    date: new Date(2022, 7, 12)
+  },{
+    id: 4,
+    title: 'New Desk',
+    price: 300,
+    date: new Date(2022, 2, 28)
+  },
+  {
+    id: 5,
+    title: 'Milk',
+    price: 450,
+    date: new Date(2019, 4, 17)
+  },
+  {
+    id: 6,
+    title: 'Air Pods',
+    price: 50,
+    date: new Date(2022, 2, 28)
+  },
+  {
+    id: 7,
+    title: 'Water',
+    price: 300,
+    date: new Date(2019, 4, 28)
+  },
+  {
+    id: 8,
+    title: 'Books',
+    price: 300,
+    date: new Date(2020, 4, 28)
+  },
+  {
+    id: 9,
+    title: 'Laptop',
+    price: 300,
+    date: new Date(2019, 8, 28)
+  },
+   {
+    id: 10,
+    title: 'Apple',
+    price: 70,
+    date: new Date(2020, 4, 18)
+  },
+  {
+    id:11,
+    title: 'Car',
+    price: 5000,
+    date: new Date(2023, 4, 18)
+  },
+]
 
 function App() {
-  const [expenses , setExpenses] = useState([
-    {
-      title: 'Fruits',
-      price: 300,
-      date: new Date()
-    },
-  
-    {
-      title: 'Food',
-      price: 400,
-      date: new Date()
-    }
-  ])
+  const [expenses , setExpenses] = useState(items)
    const addNewExpenseHandler = (data) => {
    const upExpenses = [...expenses];
    upExpenses.push(data)
--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "../../UI/expenseItem/ExpenseItem";
+import ExpensesFilter from "../expensesfilter/ExpensesFilter";
 import "./Expenses.css";
+import Chart from "../Chart/Chart";
 const Expenses = ({ expenses }) => {
+  const [selectedYear, setSelectedYear] = useState("2023");
+  const yearChangeHandler = (event) => {
+    setSelectedYear(event.target.value);
+  };
+  const filteredItems = expenses.filter((elem) => {
+    const stringifiedYear = new Date(elem.date).getFullYear().toString();
+    return stringifiedYear === selectedYear;
+  });
   return (
-    <ul className="ul-list">
-      {expenses.map((elem) => {
-        return (
-          <ExpenseItem
-            title={elem.title}
-            date={elem.date}
-            price={elem.price}
-            key={elem.title + new Date().getTime}
-          />
-        );
-      })}
-    </ul>
+    <div className="expenses">
+      <Chart items={filteredItems}/>
+      <ExpensesFilter value={selectedYear} onChange={yearChangeHandler} />
+
+      <ul className="ul-list">
+        {filteredItems.map((elem) => {
+          return (
+            <ExpenseItem
+              title={elem.title}
+              date={elem.date}
+              price={elem.price}
+              key={elem.id + new Date().getTime}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
